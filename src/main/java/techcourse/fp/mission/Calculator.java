17335f86fb3e626414d84c43986eeb6481f057af
YJGwon <yj970125@gmail.com>
2022-03-25 11:48:13 +0900

    feat: 미션 구현

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -1,32 +1,31 @@
 package techcourse.fp.mission;
 
 import java.util.List;
+import java.util.function.IntPredicate;
 
 public class Calculator {
-
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, value -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
+        return sumAll(numbers, value -> value % 2 == 0);
+
+    }
+
+    public static int sumAllOverThree(List<Integer> numbers) {
+        return sumAll(numbers, value -> value > 3);
+
+    }
+
+    private static int sumAll(List<Integer> numbers, IntPredicate predicate) {
         int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
+        for (Integer number : numbers) {
+            if (predicate.test(number)) {
                 total += number;
             }
         }
         return total;
     }
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-
-        //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
-
-        return total;
-    }
 }
